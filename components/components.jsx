0149117269d76f7b620483e9425adee72c3b1ec6
Sam Corcos <sam@corcos.io>
2015-08-22 12:22:44 +0200

    update

--- components/components.jsx
+++ components/components.jsx
@@ -8,14 +8,6 @@ if (Meteor.isClient) {
 
   function renderHouston() {
     $(document).ready(function() {
-      HoustonBody = React.createClass({
-        render() {
-          return (
-            <iframe id="houston-iframe"></iframe>
-          )
-        }
-      })
-
       React.render(
         <HoustonBody />,
         document.getElementsByTagName('body')[0]
@@ -23,3 +15,31 @@ if (Meteor.isClient) {
     })
   }
 }
+
+HoustonBody = React.createClass({
+  componentDidMount() {
+    let doc = this.getDOMNode().contentDocument
+    doc.write(<HoustonSidebar />)
+  },
+  render() {
+    return (
+      <iframe id="houston-iframe"></iframe>
+    )
+  }
+})
+
+HoustonSidebar = React.createClass({
+  render() {
+    return (
+      <h1>test</h1>
+    )
+  }
+})
+//
+// componentDidMount: function() {
+//     var doc = this.getDOMNode().contentDocument,
+//         children = React.renderComponentToString(this.props.children);
+//
+//     doc.open();
+//     doc.write(children);
+//     doc.close();
--- package.js
+++ package.js
@@ -13,6 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
   api.addFiles([
+    'iframe.js',
     'iframe.css',
     'components/components.jsx'
   ]);
